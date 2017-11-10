7cf3d97f366cd466bee7137fcab3928474b45d93
Laila BEN-ABDELAZIZ <laila.ben-abdelaziz@dps.local>
2017-11-10 15:20:45 +0100

    create my project finally!

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import MesureFilter from './Components/MesureFilter'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <MesureFilter/>
+      </div>
+    );
+  }
+}
+
+export default App;
